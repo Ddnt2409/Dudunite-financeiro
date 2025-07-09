4d929247b8660f72791ed48947f8397fe00d3b31
Ddnt2409 <Contato.dudunite@gmail.com>
2025-07-09 12:03:03 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,6 @@
-// vite.config.js - estrutura inicial do módulo financeiro
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
